8df43e9d97f0c076ee23b384f7bda13ab0abaf31
Viacheslav Matrizaev <matrizaev@gmail.com>
2024-11-21 18:25:15 +0300

    smaller modal for contacts

--- src/pages/contacts.rs
+++ src/pages/contacts.rs
@@ -6,13 +6,13 @@ use leptos::*;
 pub fn ContactsModal() -> impl IntoView {
     view! {
         <div class="modal fade" id="contactsModal" tabindex="-1" aria-labelledby="contactsModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-lg">
+            <div class="modal-dialog modal-md">
                 <div class="modal-content">
                     <div class="modal-header">
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body bg-light">
-                    <iframe class="w-100 mt-3" style="height: 80vh;" src=make_backend_url(&format!("{}{}", env::APP_CONTACTS_URL, env::APP_SIGNIN_CLIENT))></iframe>
+                        <iframe class="w-100 mt-3" style="height: 60vh;" src=make_backend_url(&format!("{}{}", env::APP_CONTACTS_URL, env::APP_SIGNIN_CLIENT))></iframe>
                     </div>
                 </div>
             </div>
